68217671b66a0a0531a8a96fcd036f74c0eed27b
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2023-10-07 09:32:52 +0300

    Makes changes to a method add

--- src/myarraylist/MyArrayList.java
+++ src/myarraylist/MyArrayList.java
@@ -2,12 +2,31 @@ package myarraylist;
 import java.util.*;
 
 public class MyArrayList <E> {
-    private E value;
-    private E[] array;
+    private Object[] array;
+    private int index;
+    public MyArrayList(){
+        array = new Object[10];
+    }
+    public void  add (E value) {
+        resize();
 
-    public E[]  add (E value, E[] array) {
-        E[] newArray = Arrays.copyOf(array, array.length+1);
-        newArray[array.length] = value;
-        return newArray;
+        array[index] = value;
+        index++;
+    }
+    private void resize() {
+        if (index == array.length) {
+            int newSize = array.length * 2;
+            Object[] newArray = new Object[newSize];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            array = newArray;
+        }
+    }
+    @Override
+    public String toString() {
+        StringJoiner result = new StringJoiner(", ");
+        for (int i = 0; i < index; i++) {
+            result.add(array[i].toString());
+        }
+        return "[" + result + "]";
     }
 }
--- src/myarraylist/MyArrayListResult.java
+++ src/myarraylist/MyArrayListResult.java
@@ -3,23 +3,28 @@ import java.util.*;
 
 public class MyArrayListResult {
     public static void main(String[] args) {
-        String[] items = {"pen", "pencil", "note", "book"};
-        String item = "paper";
-
-        Integer[] numbers = {10, 15, 8, 2};
-        Integer number = 12;
-
         MyArrayList<String> myArrayListString = new MyArrayList<>();
-        String[] arrayString = myArrayListString.add(item, items);
+        System.out.println("Результат роботи з типом String:");
+        System.out.println("Створення масиву: " + myArrayListString);
+
+        myArrayListString.add("pen");
+        myArrayListString.add("pencil");
+        myArrayListString.add("book");
+        System.out.println("Додавання елементів в масив: " + myArrayListString);
+
+        System.out.println("-------------------------------------------------");
 
         MyArrayList<Integer> myArrayListInteger = new MyArrayList<>();
-        Integer[] arrayInteger = myArrayListInteger.add(number, numbers);
+        System.out.println("Результат роботи з типом int:");
+        System.out.println("Створення масиву: " + myArrayListInteger);
 
-        System.out.println("Результат роботи методу add:");
-        System.out.println("Початковий масив строк:" + Arrays.toString(items));
-        System.out.println("Результат додавання:" + Arrays.toString(arrayString));
-        System.out.println("Початковий масив чисел:" + Arrays.toString(numbers));
-        System.out.println("Результат додавання:" + Arrays.toString(arrayInteger));
+        for (int i = 5; i < 10; i++) {
+            myArrayListInteger.add(i);
+        }
+        System.out.println("Додавання елементів в масив: " + myArrayListInteger);
+
+
+
 
     }
 }
