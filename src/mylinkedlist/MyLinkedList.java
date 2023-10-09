5b3ee1bc88a7a071db4ddcd5526acf521d248286
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2023-10-09 21:42:16 +0300

    Adds method add to class MyLinkedList

--- src/mylinkedlist/MyLinkedList.java
+++ src/mylinkedlist/MyLinkedList.java
@@ -1,5 +1,22 @@
 package mylinkedlist;
 
-public class MyLinkedList {
+public class MyLinkedList <E> {
+    private Node<E> element;
+
+    public void add (E value) {
+        Node<E> node = new Node<>();
+        node.setValue(value);
+
+        if (element == null) {
+            element = node;
+        } else {
+            Node<E> lastElement = element;
+            while (lastElement.getNext() != null) {
+                lastElement = lastElement.getNext();
+            }
+            lastElement.setNext(node);
+        }
+
+    }
 
 }
--- src/mylinkedlist/MyLinkedListResult.java
+++ src/mylinkedlist/MyLinkedListResult.java
@@ -3,5 +3,6 @@ package mylinkedlist;
 public class MyLinkedListResult {
     public static void main(String[] args) {
 
+
     }
 }
